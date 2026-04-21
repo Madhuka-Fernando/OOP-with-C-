ac61e6d82f9af0f6d2df3027e6cb447c10650245
Madhuka Fernando <info.madhukafdo@gmail.com>
2026-04-21 22:04:59 +0530

    Lab01

---
+++ Lab01/Lab01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class lab1
+{
+    public static void Main(string[] arg)
+    {
+        int width;
+        int height;
+        int area;
+        
+        Console.Write("Enter the width : ");
+        width = Int16.Parse(Console.ReadLine());
+        
+        Console.Write("Enter the height : ");
+        height = Int16.Parse(Console.ReadLine());
+
+        area = width * height;
+        
+        Console.WriteLine($"Area is {area}");
+    }
+}
