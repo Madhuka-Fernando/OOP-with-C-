200474255726ddfab09501a7aee5c10600c5a8aa
Madhuka Fernando <info.madhukafdo@gmail.com>
2026-04-28 16:51:11 +0530

    today

---
+++ Lab02/Q1/Q1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Q1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the 1st number : ");
+        int num1 = Int32.Parse(Console.ReadLine());
+
+        Console.Write("Enter the 2nd number : ");
+        int num2 = Int32.Parse(Console.ReadLine());
+
+
+        if (num2 == 0)
+        {
+            Console.WriteLine("Division by zero is not allowed . ");
+        }
+        else
+        {
+            if (num1 % num2 == 0)
+            {
+                Console.WriteLine($"{num1} is divisible by {num2}");
+            }
+            else
+            {
+                Console.WriteLine($"{num1} is not divisible by {num2}");
+            }
+        }
+
+        Console.ReadLine();
+        
+    }
+}
+
