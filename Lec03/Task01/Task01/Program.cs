035be660cbeeda7001c8b135b6fc66f833de1736
Madhuka Fernando <info.madhukafdo@gmail.com>
2026-04-29 13:58:27 +0530

    lec03

---
+++ Lec03/Task01/Task01/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Task01;
+
+class Vehicle
+{
+    public string brand;
+    public int wheels;
+
+    public void sound()
+    {
+        Console.WriteLine("Bruuum Bruuuuuuuuuuuuuuuuuuuuuuum Bruuuuuuuuuuuuuuuuuuuuuuuuuum");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Vehicle car = new Vehicle();
+        car.sound();
+        car.wheels = 3;
+
+        Console.WriteLine(car.wheels);
+
+        Vehicle van = new Vehicle();
+        van.sound();
+        van.wheels = 6;
+
+        Console.WriteLine(van.wheels);
+
+        Console.ReadLine();
+
+
+
+    }
+
+}
+
