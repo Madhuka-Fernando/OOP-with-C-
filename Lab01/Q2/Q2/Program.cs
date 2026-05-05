b08ebe37c92af20b1ae201d436bce37976387ba3
Madhuka Fernando <info.madhukafdo@gmail.com>
2026-05-05 11:22:38 +0530

    new

---
+++ Lab01/Q2/Q2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Q2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter number 1 : ");
+        int num1 = Int32.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter number 2 : ");
+        int num2 = Int32.Parse(Console.ReadLine());
+
+        int tot = num1 + num2;
+
+        Console.WriteLine($"Final Answer: {tot}");
+
+        Console.Read();
+
+    }
+}
+
