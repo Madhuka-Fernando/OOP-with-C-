fcc482fce2cea787351dc48e1a120faabe437329
Madhuka Fernando <info.madhukafdo@gmail.com>
2026-06-03 19:02:42 +0530

    lec 07 q1

---
+++ Lec07/Q1/Q1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Create a class called animal. which is the base class of dog.
+//if every animal has a behavior called speak and an attribute called name impliment then in the bellow code.
+//In the program create a dog name scooby when ever its speak it will say "scooby dooby dooo" . Impliment this funtinality in the bellow code create a separate driver class and a main method to impliment this functinality.
+
+public class animal
+{
+    public void speak()
+    {
+        
+    }
+
+    public string name;
+}
+
+public class dog : animal
+{
+
+}
